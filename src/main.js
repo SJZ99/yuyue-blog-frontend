806b5f3d6b268e303fc293908ef1321813a9fd06
YuYue <9987jdvrb@gmail.com>
2022-01-29 18:53:40 +0800

    add: vuex without config

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import vuetify from './vuetify'
 import router from './router'
 import VueRamda from 'vue-ramda'
+import store from './store'
 
 Vue.config.productionTip = false
 Vue.use(VueRamda)
@@ -11,8 +12,9 @@ Vue.use(VueRamda)
 var vm = new Vue({
   vuetify,
   router,
+  store,
   render: h => h(App),
-  mounted: () => document.dispatchEvent(new Event("x-app-rendered")),
+  mounted: () => document.dispatchEvent(new Event("x-app-rendered"))
 })
 
 vm.$mount('#app')
